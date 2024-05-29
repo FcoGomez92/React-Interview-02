7254d93e5be2e20a3ce1493157628d3b9926dad2
Francisco Manuel Gómez Gallardo <francis.gg48@gmail.com>
2024-05-29 18:13:12 +0200

    add APIData type

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,4 +1,4 @@
-import { type User } from '../types.d'
+import { type User, type APIData } from '../types.d'
 const RESULTS_NUMBER = 100
 const API_URL = `https://randomuser.me/api/?seed=FcoGomez&results=${RESULTS_NUMBER}`
 
@@ -7,6 +7,6 @@ export const fetchUsers = async (): Promise<User[]> => {
   if (!res.ok) {
     throw new Error('Error fetching data.')
   }
-  const data = await res.json()
+  const data = await res.json() as APIData
   return data.results
 }
